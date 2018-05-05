bb989ec4a0574343bf175f8b6c71fa92a7eb5134
Eduardo Aparicio Cardenes <eduardo.aparicio.cardenes@gmail.com>
2018-05-05 16:40:32 +0200

    GH-3: Improve styles JSON

--- src/functions/renderUserPosts.js
+++ src/functions/renderUserPosts.js
@@ -1,6 +1,6 @@
 // Step 2: Display on the browser
 const renderUserPosts = userWithPosts => {
-  const userJsonParagraph = document.createElement('p');
+  const userJsonParagraph = document.createElement('code');
   const text = JSON.stringify(userWithPosts, null, 2);
   const textNode = document.createTextNode(text);
   userJsonParagraph.appendChild(textNode);
